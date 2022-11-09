69fec2efd2c2040a5c5daa820af46267761bcd9d
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-11-09 06:39:09 -0300

    - add comments (--)

--- src/main/kotlin/Lexer.kt
+++ src/main/kotlin/Lexer.kt
@@ -10,21 +10,43 @@ class Lexer (name_: String, reader_: StringReader) {
     fun read (): Triple<Char?, Int, Int> {
         while (true) {
             val n1 = this.reader.read()
-            if (n1 == -1) {
+            val x1 = n1.toChar()
+            if (n1==-1 || n1==65535) {  // TODO: reads 65535 after reading -1 for the first time
                 return Triple(null, this.lin, this.col)
             }
-            val x1 = n1.toChar()
             val (l, c) = Pair(this.lin, this.col)
             when (x1) {
                 '\n' -> {
                     this.lin += 1
                     this.col = 1
                 }
-
                 ' ' -> {
                     this.col += 1
                 }
-
+                '-' -> {
+                    val n2 = this.reader.read()
+                    val x2 = n2.toChar()
+                    if (n2==-1 || x2!='-') {
+                        val c = this.col
+                        this.col += 1
+                        this.reader.unread(n2)
+                        return Triple('-', this.lin, c)
+                    }
+                    // found comment '--': ignore everything up to \n or EOF
+                    while (true) {
+                        this.col += 1
+                        val n3 = this.reader.read()
+                        val x3 = n3.toChar()
+                        if (n3 == -1) {     // EOF stops comment
+                            return Triple(null, this.lin, this.col)
+                        }
+                        if (x3 == '\n') {   // LN stops comment
+                            this.lin += 1
+                            this.col = 1
+                            break
+                        }
+                    }
+                }
                 else -> {
                     this.col += 1
                     return Triple(x1, l, c)
@@ -38,7 +60,7 @@ class Lexer (name_: String, reader_: StringReader) {
             var (x1, l1, c1) = read()
             when {
                 (x1 == null) -> break
-                (x1 in listOf('{','}', '(',')', '[',']', ',',';','=')) -> yield(Tk.Fix(x1.toString(), l1, c1))
+                (x1 in listOf('{','}', '(',')', '[',']', ',',';','=','-')) -> yield(Tk.Fix(x1.toString(), l1, c1))
                 x1.isLetter() -> {
                     var pay = ""
                     var n1 = -1
@@ -83,7 +105,7 @@ class Lexer (name_: String, reader_: StringReader) {
                 }
 
                 else -> {
-                    TODO(x1.toString())
+                    TODO("$x1 - $l1 - $c1")
                 }
             }
         }
--- src/test/kotlin/TLexer.kt
+++ src/test/kotlin/TLexer.kt
@@ -2,8 +2,8 @@ import org.junit.Test
 
 class TLexer {
     @Test
-    fun syms () {
-        val lexer = Lexer("anon", "{ } ( ; ( = ) ) , ][".reader())
+    fun syms() {
+        val lexer = Lexer("anon", "{ } ( ; ( = ) ) - , ][".reader())
         val tks = lexer.lex().iterator()
         assert(tks.next().str == "{")
         assert(tks.next().str == "}")
@@ -13,17 +13,22 @@ class TLexer {
         assert(tks.next().str == "=")
         assert(tks.next().str == ")")
         assert(tks.next().str == ")")
+        assert(tks.next().str == "-")
         assert(tks.next().str == ",")
         assert(tks.next().str == "]")
         assert(tks.next().str == "[")
         assert(tks.next() is Tk.Eof)
         assert(!tks.hasNext())
     }
+
     @Test
-    fun ids () {
-        val lexer = Lexer("anon", " if aaa throw nil XXX set loop vary10 catch else var do ado true func b10 false break".reader())
+    fun ids() {
+        val lexer = Lexer(
+            "anon",
+            " if aaa throw nil XXX set loop vary10 catch else var do ado true func b10 false break".reader()
+        )
         val tks = lexer.lex().iterator()
-        assert(tks.next().let { it is Tk.Fix && it.str=="if" })
+        assert(tks.next().let { it is Tk.Fix && it.str == "if" })
         assert(tks.next().str == "aaa")
         assert(tks.next().str == "throw")
         assert(tks.next().str == "nil")
@@ -43,5 +48,29 @@ class TLexer {
         assert(tks.next().str == "break")
         assert(tks.next() is Tk.Eof)
         assert(!tks.hasNext())
+    }
+
+    @Test
+    fun comments() {
+        val lexer = Lexer(
+            "anon", """
+            x - y - z --
+            var --x
+            --
+            val --- x
+            -- -
+            -
+        """.trimIndent().reader()
+        )
+        val tks = lexer.lex().iterator()
+        assert(tks.next().let { it is Tk.Id  && it.lin==1 && it.col==1 && it.str == "x" })
+        assert(tks.next().let { it is Tk.Fix && it.lin==1 && it.col==3 && it.str == "-" })
+        assert(tks.next().let { it is Tk.Id  && it.lin==1 && it.col==5 && it.str == "y" })
+        assert(tks.next().let { it is Tk.Fix && it.lin==1 && it.col==7 && it.str == "-" })
+        assert(tks.next().let { it is Tk.Id  && it.lin==1 && it.col==9 && it.str == "z" })
+        assert(tks.next().let { it is Tk.Fix && it.lin==2 && it.col==1 && it.str == "var" })
+        assert(tks.next().let { it is Tk.Id  && it.lin==4 && it.col==1 && it.str == "val" })
+        assert(tks.next().let { it is Tk.Fix && it.lin==6 && it.col==1 && it.str == "-" })
+        assert(tks.next().let { it is Tk.Eof && it.lin==6 && it.col==2 })
     }
 }
