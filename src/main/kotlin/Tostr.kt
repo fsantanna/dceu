d3914d3806360d48f3e32d5761190b0bb73da486
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-02-21 23:24:13 -0300

    ...

--- src/main/kotlin/Tostr.kt
+++ src/main/kotlin/Tostr.kt
@@ -22,7 +22,7 @@ fun Expr.tostr (pre: Boolean = false): String {
             }
             this.tk.str + " (" + args + ") " + task + this.body.tostr(pre)
         }
-        is Expr.Do     -> (this.tk.str=="do").cond{"do "} + (this.tag?.str ?: "") + "{\n" + this.es.tostr(pre) + "}"
+        is Expr.Do     -> (this.tk.str=="do").cond{"do "} + (this.tag.let { if (it==null) "" else it.str+" "}) + "{\n" + this.es.tostr(pre) + "}"
         is Expr.Dcl    -> /*this.poly.cond { "poly " } +*/ this.tk_.str + " " + this.id.tostr() + this.tmp.cond { " :tmp" } + this.tag.cond{" "+it.str} + this.src.cond { " = ${it.tostr(pre)}" }
         is Expr.Set    -> "set " + this.dst.tostr(pre) + " = " + this.src.tostr(pre)
         is Expr.If     -> "if " + this.cnd.tostr(pre) + " " + this.t.tostr(pre) + " else " + this.f.tostr(pre)
