8b4a1140119b1cef302f5e4ada1c515748f8b5ca
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-02-27 19:44:09 -0300

    - iterator do not assert :Iterator

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -423,7 +423,7 @@ class Parser (lexer_: Lexer)
                             val l = this.nest("""
                                 ${pre0}do {
                                     val ceu_it_$N :Iterator = ${iter.tostr(true)}
-                                    assert(ceu_it_$N is :Iterator, "expected :Iterator")
+                                    ;;assert(ceu_it_$N is :Iterator, "expected :Iterator")
                                     loop {
                                         val ${i.str} = ceu_it_$N.f(ceu_it_$N)
                                         if ${i.str} /= nil {
