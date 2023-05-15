1f1415b97f5f06e74827737e6bc2e88e96cb325a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-05-15 14:25:42 -0300

    ...

--- src/test/kotlin/ceu/TExec.kt
+++ src/test/kotlin/ceu/TExec.kt
@@ -2729,7 +2729,7 @@ class TExec {
             println(tags(t,:T), tags(t,:T.S))
             println(tags(s,:T), tags(s,:T.S))
         """, true)
-        assert(out == "31\t287\ntrue\tfalse\ntrue\ttrue\n") { out }
+        assert(out == "32\t288\ntrue\tfalse\ntrue\ttrue\n") { out }
     }
     @Test
     fun tags12() {
@@ -2821,7 +2821,7 @@ class TExec {
                 to-number(:depois)
             )
         """, true)
-        assert(out == "31\t1000\t1001\t1002\t10\t11\t12\t32\t100\t101\t33\n") { out }
+        assert(out == "32\t1000\t1001\t1002\t10\t11\t12\t33\t100\t101\t34\n") { out }
     }
     @Test
     fun enum02() {
