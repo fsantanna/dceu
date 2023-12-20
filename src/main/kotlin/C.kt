ec41fa6634e9adfb11df9ae9b3ac4861f494bc1b
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-12-20 10:32:21 -0300

    ...

--- src/main/kotlin/C.kt
+++ src/main/kotlin/C.kt
@@ -1502,7 +1502,6 @@ fun Coder.main (tags: Tags): String {
     #if CEU >= 5
                 case CEU_VALUE_EXE_TASK_IN:
     #endif
-                    assert(cur->Exe_Task.status != CEU_EXE_STATUS_TERMINATED);
                     CEU_Bstk xstk = { cur->Exe_Task.dn_block, 1, bstk };
                     CEU_Value ret = ceu_bcast_dyns(&xstk, cur->Any.hld.prev, evt);
                     if (!bstk->on || !xstk.on) {
