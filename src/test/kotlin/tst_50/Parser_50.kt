fac8c1c50c696c752cfa209ddf4afe60b2daeb58
Francisco Sant'Anna <francisco.santanna@gmail.com>
2024-08-23 19:32:51 -0300

    ...

--- src/test/kotlin/tst_50/Parser_50.kt
+++ src/test/kotlin/tst_50/Parser_50.kt
@@ -28,5 +28,21 @@ class Parser_50 {
         //assert(e.tostr() == "(coro :nested () {\nnil\n})") { e.tostr() }
         assert(trap { parser.expr() } == "anon : (lin 2, col 18) : expected \"(\" : have \":nested\"")
     }
+    @Test
+    fun aa_03_task_err() {
+        val l = lexer("""
+            task :xxx () {}
+        """.trimIndent())
+        val parser = Parser(l)
+        //assert(trap { parser.expr() } == "anon : (lin 1, col 1) : invalid task : unexpected \":xxx\"")
+        assert(trap { parser.expr() } == "anon : (lin 1, col 6) : expected \"(\" : have \":xxx\"")
+    }
+    @Test
+    fun aa_04_task() {
+        val l = lexer("task (a,b) :nested { 10 }")
+        val parser = Parser(l)
+        val e = parser.expr()
+        assert(e.tostr() == "(task (a,b) :nested {\n10;\n})") { e.tostr() }
+    }
 
 }
