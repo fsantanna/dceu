0c92c89c86064e40ae901a176049728fdd1ea23c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2024-09-12 12:46:37 -0300

    - tags

--- src/main/kotlin/Tags.kt
+++ src/main/kotlin/Tags.kt
@@ -1,11 +1,10 @@
 package dceu
 
 class Tags (outer: Expr.Do) {
-    val xxx: Map<String,Tk.Tag>
-    val pub: Map<String,Pair<String,String>>
+    val pub: Map<String,Tk.Tag>
 
     init {
-        this.xxx = TAGS.map { Pair(it,Tk.Tag(it,outer.tk.pos.copy())) }.toMap() + outer.dn_gather {
+        this.pub = TAGS.map { Pair(it,Tk.Tag(it,outer.tk.pos.copy())) }.toMap() + outer.dn_gather {
             when (it) {
                 is Expr.Do     -> if (it.tag == null) emptyMap() else mapOf(Pair(it.tag.str,it.tag))
                 is Expr.Escape -> mapOf(Pair(it.tag.str,it.tag))
@@ -15,12 +14,11 @@ class Tags (outer: Expr.Do) {
                 else           -> emptyMap()
             }
         }
-        this.pub = this.xxx.map { (id,tk)->Pair(id,Pair(id,id.idc())) }.toMap()
-        for ((id,tk) in this.xxx) {
+        for ((id,tk) in this.pub) {
             val issub = id.contains('.')
             //println(listOf(id,issub))
             val sup = id.dropLastWhile { it != '.' }.dropLast(1)
-            if (issub && !this.xxx.containsKey(sup)) {
+            if (issub && !this.pub.containsKey(sup)) {
                 err(tk, "tag error : parent tag $sup is not declared")
             }
         }
